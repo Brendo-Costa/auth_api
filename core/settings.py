fadf39c292dc77a4f170c7d7e3a0d17ef5d99fab
Brendo Costa <aldenir.brendo@gmail.com>
2023-05-05 14:11:31 -0300

    feat: implementing tests for admin and model User

--- core/settings.py
+++ core/settings.py
@@ -135,3 +135,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#This are the oficial user model 
+AUTH_USER_MODEL = 'auth_api.User'
